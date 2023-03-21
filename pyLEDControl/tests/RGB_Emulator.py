2dd1329c21ffa1588ad42f33ea2aa9b83fcf7ec1
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-03-21 22:02:02 +0100

    project: create rgb-emulator for testing

--- pyLEDControl/tests/RGB_Emulator.py
+++ pyLEDControl/tests/RGB_Emulator.py
@@ -1,2 +1,30 @@
+import sys
+import time
 from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
 
+class RGB_Emulator():
+    def __init__(self) -> None:
+        self.mode:callable = None
+
+    def run(self):
+        options = RGBMatrixOptions()
+        options.pixel_size=16
+        options.pixel_style="round"
+        options.rows=64
+        options.cols=128
+        matrix= RGBMatrix(options=options)
+        while 1:
+            try:
+                matrix.Clear()
+                if self.mode != None: self.mode(matrix) 
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print("Exiting\n")
+                sys.exit(0)
+
+if __name__ == "__main__":
+    matrix = RGB_Emulator()
+    def pixel(matrix:RGBMatrix):
+        matrix.SetPixel(1,1,10,20,30)
+    matrix.mode=pixel
+    matrix.run()
