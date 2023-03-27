14522e5f1300dc4fe56fd8f12b4a6e7a7141c610
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-03-27 22:24:39 +0200

    Extend the LedController

--- pyLEDControl/control/led_controller.py
+++ pyLEDControl/control/led_controller.py
@@ -1,3 +1,21 @@
+import settings
+from misc.domain_data import ExecutionMode
+from display.RGB_Emulator import RgbEmulator
+from multiprocessing import Process
+
+
 class LedController():
-    def __init__(self) -> None:
+    matrix: RgbEmulator
+
+    def start_emulator(self) -> None:
+        self.matrix = RgbEmulator()
+        # TODO: make the rgb-emulator a "multiprocessing" process and use a shared variable
+
+    def start_rgb_matrix(self) -> None:
         pass
+
+    def __init__(self) -> None:
+        if settings.ExecutionMode == ExecutionMode.EMULATED:
+            self.start_emulator()
+        else:
+            self.start_rgb_matrix()
