4b29a7b5cedf2b0958dae3f69501fc91e047eb28
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-04-03 21:41:07 +0200

    Cleanup

--- pyLEDControl/misc/data_manager.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from multiprocessing.managers import BaseManager, Namespace
-
-
-class DataManager(BaseManager):
-    pass
--- pyLEDControl/run.py
+++ pyLEDControl/run.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from misc.logging import Log
-import multiprocessing.managers
-from multiprocessing import Value, Manager, Queue
+from multiprocessing import Queue
 import time
 from server.server import Server
 from control.led_controller import LedController
-from control.effect_message import EffectMessage
-from misc.data_manager import DataManager
 
 
 def main():
