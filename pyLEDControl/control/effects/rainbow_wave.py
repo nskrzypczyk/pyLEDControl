49c7bebcec009a101118a0485bf7dd2ca4251857
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-04-11 23:14:44 +0200

    Rainbow wave effect

--- pyLEDControl/control/effects/rainbow_wave.py
+++ pyLEDControl/control/effects/rainbow_wave.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import math
+import colorsys
+import time
+from misc.utils import rotate
 import settings
 from typing import Literal
 from dataclasses import dataclass
 from control.adapter.abstract_matrix import AbstractMatrix
 from control.effects.abstract_effect import AbstractEffect
+from misc.logging import Log
+
+log = Log("RainbowWave")
 
 
 class RainbowWave(AbstractEffect):
@@ -21,11 +26,17 @@ class RainbowWave(AbstractEffect):
     def run(matrix: AbstractMatrix, options: EffectOptions = default_options):
         max_width = settings.MATRIX_EMULATION.WIDTH.value-1
         max_height = settings.MATRIX_EMULATION.HEIGHT.value-1
-        rainbow = [(255, 0, 0), (255, 127, 0), (255, 255, 0),
-                   (0, 255, 0), (0, 0, 255), (75, 0, 130), (148, 0, 211)]
+        rainbow = []
+        for i in range(128):
+            hue = 0 + i*(1/128)
+            r, g, b = [int(255 * c)
+                       for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+            rainbow.append((r, g, b))
 
+        log.debug(f"Color list length: {len(rainbow)}")
         starting_point: tuple[int, int]  # (x,y)
 
+        # FIXME: Implement options
         if options.corner == "lt":
             starting_point = (0, 0)
         elif options.corner == "rt":
@@ -36,6 +47,27 @@ class RainbowWave(AbstractEffect):
             starting_point = (0, max_height)
 
         matrix.Clear()
-        offset_canvas = matrix.CreateFrameCanvas()
+        canvas: AbstractMatrix = matrix.CreateFrameCanvas()
+        base_offset = 1
         while 1:
-            pass
+            for x in range(max_width+1):
+                base_x = x
+                base_y = 0
+                while base_x > -1 and base_y <= max_height:
+                    canvas.SetPixel(
+                        base_x, base_y, rainbow[x][0], rainbow[x][1], rainbow[x][2])
+                    base_x -= 1
+                    base_y += 1
+            counter = 1
+            for x in range(max_width+1):
+                base_x = x
+                base_y = max_height
+                while base_x <= max_width and base_y > -1:
+                    canvas.SetPixel(
+                        base_x, base_y, rainbow[max_height+counter][0], rainbow[max_height+counter][1], rainbow[max_height+counter][2])
+                    base_x += 1
+                    base_y -= 1
+                counter += 1
+            time.sleep(0.005)
+            canvas = matrix.SwapOnVSync(canvas)
+            rainbow = rotate(rainbow, base_offset)
