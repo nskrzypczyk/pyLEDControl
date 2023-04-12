45b4fc9cd989f4c7259bf76b1066a68086aa4904
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-04-12 23:42:25 +0200

    Extended rainbow wave

--- pyLEDControl/control/effects/rainbow_wave.py
+++ pyLEDControl/control/effects/rainbow_wave.py
@@ -10,46 +10,52 @@ from control.adapter.abstract_matrix import AbstractMatrix
 from control.effects.abstract_effect import AbstractEffect
 from misc.logging import Log
 
+# FIXME: Implement options
+
 log = Log("RainbowWave")
+max_width = settings.MATRIX_EMULATION.WIDTH.value-1
+max_height = settings.MATRIX_EMULATION.HEIGHT.value-1
 
 
 class RainbowWave(AbstractEffect):
+    rainbow = []
+    for i in range(128):
+        hue = 0 + i*(1/128)
+        r, g, b = [int(255 * c)
+                   for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+        rainbow.append((r, g, b))
 
     @dataclass
     class EffectOptions():
 
-        corner: Literal["lt", "rt", "lb", "rb"] = "lt"
+        mode: Literal["left to right",
+                      "top left to bottom right"] = "top left to bottom right"
 
-    default_options: EffectOptions = EffectOptions("lt")
+    default_options: EffectOptions = EffectOptions()
 
     @staticmethod
-    def run(matrix: AbstractMatrix, options: EffectOptions = default_options):
-        max_width = settings.MATRIX_EMULATION.WIDTH.value-1
-        max_height = settings.MATRIX_EMULATION.HEIGHT.value-1
-        rainbow = []
-        for i in range(128):
-            hue = 0 + i*(1/128)
-            r, g, b = [int(255 * c)
-                       for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
-            rainbow.append((r, g, b))
-
-        log.debug(f"Color list length: {len(rainbow)}")
-        starting_point: tuple[int, int]  # (x,y)
-
-        # FIXME: Implement options
-        if options.corner == "lt":
-            starting_point = (0, 0)
-        elif options.corner == "rt":
-            starting_point = (max_width, 0)
-        elif options.corner == "rb":
-            starting_point = (max_width, max_height)
-        elif options.corner == "lb":
-            starting_point = (0, max_height)
-
+    def left_to_right(matrix: AbstractMatrix):
+        rainbow = RainbowWave.rainbow
         matrix.Clear()
         canvas: AbstractMatrix = matrix.CreateFrameCanvas()
         base_offset = 1
         while 1:
+            for x in range(max_width+1):
+                [canvas.SetPixel(x, y, rainbow[x][0], rainbow[x][1],
+                                 rainbow[x][2]) for y in range(max_height+1)]
+
+            time.sleep(0.005)
+            canvas = matrix.SwapOnVSync(canvas)
+            rainbow = rotate(rainbow, base_offset)
+
+    @staticmethod
+    def top_left_to_bottom_right(matrix: AbstractMatrix):
+        rainbow = RainbowWave.rainbow
+        matrix.Clear()
+        canvas: AbstractMatrix = matrix.CreateFrameCanvas()
+        base_offset = 1
+        while 1:
+            counter = 1
             for x in range(max_width+1):
                 base_x = x
                 base_y = 0
@@ -58,8 +64,7 @@ class RainbowWave(AbstractEffect):
                         base_x, base_y, rainbow[x][0], rainbow[x][1], rainbow[x][2])
                     base_x -= 1
                     base_y += 1
-            counter = 1
-            for x in range(max_width+1):
+
                 base_x = x
                 base_y = max_height
                 while base_x <= max_width and base_y > -1:
@@ -68,6 +73,14 @@ class RainbowWave(AbstractEffect):
                     base_x += 1
                     base_y -= 1
                 counter += 1
+
             time.sleep(0.005)
             canvas = matrix.SwapOnVSync(canvas)
             rainbow = rotate(rainbow, base_offset)
+
+    @staticmethod
+    def run(matrix: AbstractMatrix, options: EffectOptions = default_options):
+        if options.mode == "left to right":
+            RainbowWave.left_to_right(matrix)
+        elif options.mode == "top left to bottom right":
+            RainbowWave.top_left_to_bottom_right(matrix)
