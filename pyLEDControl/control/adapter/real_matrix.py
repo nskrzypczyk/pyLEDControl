20e043d49bbac9ceef63dd8c00794d891d798d90
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-04-15 20:42:10 +0200

    create class for the real matrix

---
+++ pyLEDControl/control/adapter/real_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from control.adapter.abstract_matrix import AbstractMatrix
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+
+
+class RealMatrix(RGBMatrix, AbstractMatrix):
+    def __init__(self, options={}):
+        super().__init__(options)
