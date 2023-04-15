093a6fa20c7cd520fc7340d0ab1c40e3718164a2
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-04-15 20:43:27 +0200

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-
+cd ..
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
 cd rpi-rgb-led-matrix
 make build-python PYTHON=$(command -v python3)
