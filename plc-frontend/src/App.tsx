29a1aaa8475c7f469682bc8a66414f62eaf2d7ad
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-05-01 23:21:58 +0200

    Frontend responsiveness

--- plc-frontend/src/App.tsx
+++ plc-frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Avatar, Box, Card, CardContent, CardHeader, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Paper, Slider, Stack, Typography, Button, Chip } from '@mui/material';
+import { Avatar, Box, Card, CardContent, CardHeader, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Paper, Slider, Stack, Typography, Button, Chip, withStyles, ChipTypeMap } from '@mui/material';
 import BrightnessHighIcon from '@mui/icons-material/BrightnessHigh';
 import { BrightnessLowRounded, SendTimeExtension } from '@mui/icons-material';
-
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 const App: React.FC = () => {
   const [brightness, setBrightness] = React.useState<number>(50);
@@ -29,70 +29,72 @@ const App: React.FC = () => {
   }
 
   return (
-    <Grid container direction="row" justifyContent={'center'} alignItems="center" spacing={6}>
-      <Grid item xs={8} />
-      <Grid item xs={8}>
-
-        <Card sx={{
-          backgroundColor: "lightgray"
-        }}>
-          <CardHeader title={
-            <Typography gutterBottom variant='h2' component="div">
-              pyLEDControl
-            </Typography>
-          }>
-          </CardHeader>
-          <CardContent>
-            <Grid container direction="row" justifyContent={'space-evenly'} alignItems="stretch" xs={12} spacing={6}>
-              <Grid item xs={6}>
-                <Box style={{ borderRadius: "12px", backgroundColor: "white", padding: "10px" }}>
-                  <Stack direction={'row'} spacing={2} alignItems="center" justifyContent={'center'}>
-                    <BrightnessHighIcon />
-                    <Typography variant='h5'>
-                      Brightness
-                    </Typography>
-                  </Stack >
-                  <Divider sx={{ mt: 1.5, mb: 1.5 }} />
-                  <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-                    <Button onClick={decreaseBrightness}>
-                      <BrightnessLowRounded />
-                    </Button>
-                    <Slider aria-label="Volume" value={brightness} onChange={handleBrigthnessChange} />
-                    <Button onClick={increaseBrightness}>
-                      <BrightnessHighIcon />
-                    </Button>
-                  </Stack>
-                  <Stack direction={'row'} spacing={2} alignItems="center" justifyContent={'center'}>
-                    <Typography variant="h6">
-                      {brightness} %
-                    </Typography>
-                  </Stack>
-                </Box>
+    <Card sx={{
+      backgroundColor: "lightgray",
+      flexGrow: 1
+    }} style={{ borderRadius: "12px" }}>
+      <CardHeader title={
+        <Typography gutterBottom variant='h3' component="div">
+          pyLEDControl
+        </Typography>
+      }>
+      </CardHeader>
+      <CardContent>
+        <Grid container direction="row" justifyContent="space-evenly" alignItems="stretch" columns={{ xs: 1, sm: 2, md: 3 }} spacing={{ xs: 2, md: 3 }}>
+          <Grid item xs={1.2}>
+            <Box style={{ borderRadius: "12px", backgroundColor: "white", padding: "10px" }}>
+              <Grid container columns={3} direction="row" alignItems="center">
+                <Grid container item xs={1} >
+                  <BrightnessHighIcon />
+                </Grid>
+                <Grid item xs="auto">
+                  <Typography variant='h5'>
+                    Brightness
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={6}>
-                <Box style={{ borderRadius: "12px", backgroundColor: "white", padding: "10px" }}>
-                  <Stack direction={'row'} spacing={2} alignItems="center" justifyContent={'center'}>
-                    <SendTimeExtension />
-                    <Typography variant='h5'>
-                      Effect
-                    </Typography>
-                  </Stack >
-                  <Divider sx={{ mt: 1.5, mb: 1.5 }} />
-                  <Stack direction="row" spacing={1} justifyContent="center">
-                    <Chip label="Spotify" />
-                    <Chip label="Wave" />
-                    <Chip label="RainbowWave" />
-                    <Chip label="RandomDot" />
-                    <Chip label="DigiClock" />
-                  </Stack>
-                </Box>
+              <Divider sx={{ mt: 1.5, mb: 1.5 }} />
+              <Stack spacing={1} direction="row" sx={{ mb: 1 }} alignItems="center">
+                <Button onClick={decreaseBrightness}>
+                  <BrightnessLowRounded />
+                </Button>
+                <Slider aria-label="Volume" value={brightness} onChange={handleBrigthnessChange} />
+                <Button onClick={increaseBrightness}>
+                  <BrightnessHighIcon />
+                </Button>
+              </Stack>
+              <Stack direction={'row'} spacing={1} alignItems="center" justifyContent={'center'}>
+                <Typography variant="h6">
+                  {brightness} %
+                </Typography>
+              </Stack>
+            </Box>
+          </Grid>
+          <Grid item xs={1} justifyContent="center">
+            <Box style={{ borderRadius: "12px", backgroundColor: "white", padding: "10px" }}>
+              <Grid container columns={8} direction="row" alignItems="center">
+                <Grid container item xs={1}>
+                  <SendTimeExtension />
+                </Grid>
+                <Grid item xs="auto">
+                  <Typography variant='h5'>
+                    Effect
+                  </Typography>
+                </Grid>
+              </Grid >
+              <Divider sx={{ mt: 1.5, mb: 1.5 }} />
+              <Grid container item spacing={1}>
+                {["Spotify", "Wave", "RainbowWave", "DigiClock", "RandomDot"].map((e) => (
+                  <Grid item>
+                    <Chip label={e} />
+                  </Grid>
+                ))}
               </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={8} />
-    </Grid>
+            </Box>
+          </Grid>
+        </Grid>
+      </CardContent >
+    </Card >
   );
 }
 
