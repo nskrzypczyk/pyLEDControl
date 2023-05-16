35f1477d072e5f0c2acdd8973d31a27adabdadde
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-05-17 00:16:21 +0200

    Frontend: fix cors

--- plc-frontend/src/api/ApiManager.ts
+++ plc-frontend/src/api/ApiManager.ts
@@ -1,5 +1,8 @@
 import { IEffectData } from '../domainData/DomainData';
 
+// const HOST = "http://127.0.0.1:8080"
+const HOST = `http://${window.location.hostname}:8080`
+
 const handleErrors = (res: Response) => {
     if (!res.ok) {
         throw new Error(res.statusText)
@@ -7,19 +10,21 @@ const handleErrors = (res: Response) => {
 }
 
 export const setEffect = async (effectData: IEffectData) => {
-    const res = await fetch(`/effect/${effectData.effect}/${effectData.brightness}`, {
+    const res = await fetch(`${HOST}/effect/${effectData.effect}/${effectData.brightness}`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
-        }
+            "Content-Type": "application/json",
+        },
+        mode: "cors"
     })
     handleErrors(res)
     return res.json()
 }
 
 export const getStatus = async () => {
-    const res = await fetch("/effect/current", {
-        method: "GET"
+    const res = await fetch(`${HOST}/effect/current`, {
+        method: "GET",
+        mode: "cors"
     })
     handleErrors(res)
     return res.json()
