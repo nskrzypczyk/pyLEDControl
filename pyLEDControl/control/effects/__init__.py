cd29d9b0dc61a2525f5079c24c25d1f382303042
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-06-08 22:37:05 +0200

    Add weather effect to effect collection

--- pyLEDControl/control/effects/__init__.py
+++ pyLEDControl/control/effects/__init__.py
@@ -4,6 +4,7 @@ from control.effects.rainbow_wave import RainbowWave
 from control.effects.digi_clock import DigiClock
 from control.effects.spotify import Spotify
 from control.effects.off import OFF
+from control.effects.weather import Weather
 import sys
 
 effects = "control.effects"
