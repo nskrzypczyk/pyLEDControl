4ed0d530ca90f3453d548c4a5439ec66c4a39bd9
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-07-17 22:41:01 +0200

    Introduce shuffle effect

--- pyLEDControl/control/effects/shuffle.py
+++ pyLEDControl/control/effects/shuffle.py
@@ -1,15 +1,44 @@
 from abc import abstractmethod
+import os
+import time
 from control.effects.abstract_effect import AbstractEffect
 from control.effect_message import EffectMessage
 from control.adapter.abstract_matrix import AbstractMatrix
-import settings
+from misc.logging import Log
+from threading import Thread
+from multiprocessing import Process
 
 
 class Shuffle(AbstractEffect):
     @abstractmethod
-    def run(matrix: type, msg: EffectMessage):
-        matrix: AbstractMatrix = matrix(options=settings.rgb_options())
-        canvas: AbstractMatrix = matrix.CreateFrameCanvas()
-        font = matrix.graphics.Font()
-        font.LoadFont("../../rpi-rgb-led-matrix/fonts/6x9.bdf")
-        refresh_rate = 5
+    def run(matrix: type, msg: EffectMessage, conn):
+        log = Log(__class__.__name__)
+        from control.effects import effect_dict
+
+        local_effect_dict = effect_dict
+        local_effect_dict.pop("OFF")
+        max_count = len(local_effect_dict)
+        counter = 0
+        while not Shuffle.is_terminated(conn):
+            if counter == max_count - 1:
+                log.debug("Resetting counter")
+                counter = 0
+
+            tt = Process(
+                target=list(local_effect_dict.values())[counter].run,
+                args=[matrix, msg, conn],
+            )
+            counter += 1
+            log.debug("Starting thread")
+            tt.start()
+            log.debug("Sleeping")
+            time.sleep(7)
+            exit_sub(tt, log)
+        exit_sub(tt, log)
+
+
+def exit_sub(tt, log):
+    while tt.is_alive():
+        log.debug("Killing child process")
+        tt.terminate()
+        tt.join()
