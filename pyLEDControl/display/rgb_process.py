4c2dd22454b2f25c6903fe54b218f07a786eb532
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-07-17 22:20:28 +0200

    MatrixProcess: Use pipe to kill processes

--- pyLEDControl/display/rgb_process.py
+++ pyLEDControl/display/rgb_process.py
@@ -1,3 +1,4 @@
+import os
 import time
 import settings
 from control.effects.abstract_effect import AbstractEffect
@@ -5,10 +6,10 @@ from control.adapter.abstract_matrix import AbstractMatrix
 from control.adapter.real_matrix import RealMatrix
 from misc.logging import Log
 from control.effect_message import EffectMessage
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Pipe
 
 
-class MatrixProcess():
+class MatrixProcess:
     def __init__(self, matrix: AbstractMatrix) -> None:
         self.matrix = matrix
         self.log: Log = Log("MatrixProcesss")
@@ -24,12 +25,15 @@ class MatrixProcess():
                     message: EffectMessage = queue.get(block=False)
                     print(message)
                     if message.effect != current_effect:
-                        self.log.debug(
-                            "Effect has changed. Restarting process")
+                        self.log.debug("Effect has changed. Restarting process")
+
                         if proc:
-                            proc.kill()
+                            conn_p.send(True)
+                            proc.join()
+                        conn_p, conn_c = Pipe(True)
                         proc = Process(
-                            target=message.effect.run, args=[matrix, message])
+                            target=message.effect.run, args=[matrix, message, conn_c]
+                        )
                         proc.start()
                         current_effect = message.effect
                 time.sleep(1)
@@ -39,12 +43,3 @@ class MatrixProcess():
     def run(self, queue: Queue):
         self.log.debug("run method called.")
         self.loop(self.matrix, queue)
-
-
-if __name__ == "__main__":
-    matrix_class_name = MatrixProcess()
-
-    def pixel(matrix: AbstractMatrix):
-        matrix.SetPixel(1, 1, 10, 20, 30)
-    matrix_class_name.mode = pixel
-    matrix_class_name.run()
