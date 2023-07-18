4722fe5eff2dd6f2d1f21ace484be3ac4189b71d
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-07-18 21:48:58 +0200

    Shuffle: fix process termination

--- pyLEDControl/control/effects/shuffle.py
+++ pyLEDControl/control/effects/shuffle.py
@@ -6,12 +6,12 @@ from control.effect_message import EffectMessage
 from control.adapter.abstract_matrix import AbstractMatrix
 from misc.logging import Log
 from threading import Thread
-from multiprocessing import Process
+from multiprocessing import Process, Pipe
 
 
 class Shuffle(AbstractEffect):
     @abstractmethod
-    def run(matrix: type, msg: EffectMessage, conn):
+    def run(matrix: type, msg: EffectMessage, conn_p):
         log = Log(__class__.__name__)
         from control.effects import effect_dict
 
@@ -19,26 +19,26 @@ class Shuffle(AbstractEffect):
         local_effect_dict.pop("OFF")
         max_count = len(local_effect_dict)
         counter = 0
-        while not Shuffle.is_terminated(conn):
+        while not Shuffle.is_terminated(conn_p):
             if counter == max_count - 1:
                 log.debug("Resetting counter")
                 counter = 0
-
+            _conn, conn_c = Pipe(True)
             tt = Process(
                 target=list(local_effect_dict.values())[counter].run,
-                args=[matrix, msg, conn],
+                args=[matrix, msg, conn_c],
             )
             counter += 1
             log.debug("Starting thread")
             tt.start()
             log.debug("Sleeping")
             time.sleep(7)
-            exit_sub(tt, log)
-        exit_sub(tt, log)
+            exit_sub(tt, log, _conn)
+        exit_sub(tt, log, _conn)
 
 
-def exit_sub(tt, log):
-    while tt.is_alive():
+def exit_sub(tt, log, conn):
+    if tt.is_alive():
         log.debug("Killing child process")
-        tt.terminate()
+        conn.send(True)
         tt.join()
