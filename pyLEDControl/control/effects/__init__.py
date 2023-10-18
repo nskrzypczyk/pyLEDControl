851b94de7f10d6949362de4bccb16e28080bec7a
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-10-18 20:16:36 +0200

    feat: dynamic import for effects in __init__

--- pyLEDControl/control/effects/__init__.py
+++ pyLEDControl/control/effects/__init__.py
@@ -1,20 +1,23 @@
-from abc import ABC
-from control.effects.wave import Wave
-from control.effects.game_of_life import GameOfLife
-from control.effects.random_dot import RandomDot
-from control.effects.rainbow_wave import RainbowWave
-from control.effects.digi_clock import DigiClock
-from control.effects.spotify import Spotify
-from control.effects.weather import Weather
-from control.effects.shuffle import Shuffle
-from control.effects.off import OFF
-import sys
+import pkgutil
+import importlib
 
-from control.effects.abstract_effect import AbstractEffect
+effect_dict = {}
+effect_list = []
 
-effects = "control.effects"
-__import__(effects)
-effect_list = sys.modules[effects]
-effect_dict: dict[str, type[AbstractEffect]] = {
-    name: T for name, T in effect_list.__dict__.items() if (isinstance(T, type) and name!="AbstractEffect") # Implement marker interface instead of hardcoded exclusion!
-}
+def get_effects():
+    global effect_dict, effect_list
+    AbstractEffect = None
+    package_name = 'control.effects'
+    abstract_class_module_name = package_name + ".abstract_effect"
+    package = importlib.import_module(package_name)
+    packages = pkgutil.walk_packages(package.__path__, prefix=package_name + '.')
+    for importer, modname, ispkg in packages:
+        module = importlib.import_module(modname)
+        if modname==abstract_class_module_name:
+            AbstractEffect = getattr(module, "AbstractEffect")
+        for name, obj in module.__dict__.items():
+            if isinstance(obj, type) and issubclass(obj, AbstractEffect) and obj is not AbstractEffect:
+                effect_dict[name] = obj
+    effect_list = list(effect_dict.keys())
+    return effect_dict
+get_effects()
