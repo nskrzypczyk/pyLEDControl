034075f88ca3e45b8d3c358390c0854288713eeb
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-10-18 20:30:56 +0200

    style: remove unnecessary print statements

--- pyLEDControl/tests/bindings/test_weather_binding.py
+++ pyLEDControl/tests/bindings/test_weather_binding.py
@@ -10,7 +10,6 @@ class TestWeatherBinding(unittest.TestCase):
 
     def test_response(self):
         res = self.weather_binding.get()
-        print(res)
         self.assertNotEquals(res, [], "Response list should not be empty")
 
     def test_get_all_icons(self):
@@ -22,6 +21,5 @@ class TestWeatherBinding(unittest.TestCase):
                     'covered': 'display/weather-icons/covered.png',
                     'shower': 'display/weather-icons/shower.png'
                     }
-        print(png_files)
         self.assertDictEqual(
             png_files, expected, "Icon dict does not match the expected value")
