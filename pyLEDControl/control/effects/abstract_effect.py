8fe710c5aac20698b3efc5338a51d20df379f139
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-10-22 22:50:24 +0200

    feat: add a way to update options while an effect is in use
    TODO: Remove old brightness control using I/O

--- pyLEDControl/control/effects/abstract_effect.py
+++ pyLEDControl/control/effects/abstract_effect.py
@@ -3,6 +3,7 @@
 
 from abc import ABC
 from dataclasses import dataclass
+from typing import Union
 
 from control.abstract_effect_options import AbstractEffectOptions
 
@@ -14,17 +15,33 @@ class AbstractEffect(ABC):
         pass
 
     @staticmethod
-    def run(matrix: type, effect_message, conn):
+    def run(matrix: type, effect_message, conn, *args, **kwargs):
         """Effect definition which will be called by MatrixProcess"""
         pass
 
     @staticmethod
-    def is_terminated(conn):
+    def is_terminated(conn) -> bool:
         """
         Uses a multiprocessing pipe connection to receive a stop signal
         which should be used to terminate the run() method.
         """
         if conn.poll():
-            return conn.recv()
+            pipe_data = conn.recv()
+            if isinstance(pipe_data,bool):
+                return pipe_data
+            else:
+                return False
         else:
             return False
+
+    @staticmethod
+    def get_new_options(conn) -> Union[None, AbstractEffectOptions]:
+        """
+        Uses a multiprocessing pipe connection to receive updated effect related options. 
+        """
+        if conn.poll():
+            pipe_data = conn.recv()
+            if isinstance(pipe_data, AbstractEffectOptions):
+                return pipe_data
+        else:
+            return None
