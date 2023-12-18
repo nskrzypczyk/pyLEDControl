8aecd7adfb0431de6f5fdf5d206f1aee4e182cfb
nskrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-12-18 22:37:19 +0100

    feat: make shuffle more stable

--- pyLEDControl/control/effects/shuffle.py
+++ pyLEDControl/control/effects/shuffle.py
@@ -24,20 +24,28 @@ class Shuffle(AbstractEffect):
 
     def run(matrix: type, options: Options, conn_p: Connection, *args, **kwargs):
         log = Log(__class__.__name__)
-
+        tt = None
         counter = 0
         while not __class__.is_terminated(conn_p):
             if counter == len(options.active_effects):
                 log.debug("Resetting counter")
                 counter = 0
             _conn, conn_c = Pipe(True)
+
+            # active_effects can run on the fly via pipes so we need to check. 
+            # introducing another variable for this matter is not necessary.
+            if counter >= len(options.active_effects): 
+                exit_sub(tt, log, _conn)
+                return Shuffle.run(matrix, options, conn_p)
             tt = Process(
                 target=get_effects()[options.active_effects[counter]].run,
                 args=[matrix, options, conn_c],
             )
+
             counter += 1
             log.debug("Starting thread")
             tt.start()
+
             log.debug("Sleeping")
             time.sleep(7)
             exit_sub(tt, log, _conn)
@@ -45,7 +53,7 @@ class Shuffle(AbstractEffect):
 
 
 def exit_sub(tt, log, conn):
-    if tt.is_alive():
+    if tt is not None and tt.is_alive():
         log.debug("Killing child process")
         conn.send(True)
         tt.join()
