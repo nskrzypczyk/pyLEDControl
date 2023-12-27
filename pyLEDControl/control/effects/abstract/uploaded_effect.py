0f6940715116c14c03bc5e77668d0a38f19cd680
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-12-27 22:32:26 +0100

    feat(#10): add basic effect for uploaded effects

---
+++ pyLEDControl/control/effects/abstract/uploaded_effect.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List
+from misc.domain_data import MultiselectConstraint
+import settings
+from control.abstract_effect_options import AbstractEffectOptions
+from control.adapter.abstract_matrix import AbstractMatrix
+from control.effects.abstract_effect import AbstractEffect
+from misc.logging import Log
+
+log = Log("UploadedEffect")
+def get_uploaded_effects() -> list:
+    pass
+
+class UploadedEffect(AbstractEffect):
+    @dataclass
+    class Options(AbstractEffectOptions):
+        active_effects: List[str]
+        active_effects_constraint = MultiselectConstraint("Active uploaded effects", get_uploaded_effects)
+    @staticmethod
+    def run(matrix_class, options, conn, *args, **kwargs):
+        matrix: AbstractMatrix = matrix_class(options=settings.rgb_options())
+        canvas: AbstractMatrix = matrix.CreateFrameCanvas()
